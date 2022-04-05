981d0a64f9cd892e19738eaeb91a9688f017e069
Eric Hagen <ericjameshagen@gmail.com>
2022-04-05 14:34:42 -0600

    added server file

--- server/server.js
+++ server/server.js
@@ -6,25 +6,42 @@ const port = 3000;
 //TODO:
 //routing methods
 //catch all route
-//configure global error handler
 
 
 
+//^^^^^ more specific route requests go above ^^^^^
+//routing methods go here
 
 //handle get requests to root directory serving index.html
 app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, '../src/index.html'))
 });
 
-//routing methods go here
+// app.post('/', (req, res) => {
+//     res.status(200).json(res.locals) //finish out post req
+// })
+app.use('/assets', express.static('./client/assets'))
 
 
-//configure global error handler
-app.use((err, req, res, next) => {
-    console.log(err)
-    res.status(500).send('Something broke')
+//catch all for invalid file paths
+app.use('*', (req, res) => {
+    console.log('not a valid filepath')
+    res.sendStatus(404);
 });
 
+//global error handler
+app.use((err, req, res, next) => {
+    console.log('global error handler invoked')
+    const defaultErr = {
+      log: 'express error handler caught error',
+      status: 400,
+      message: { err: 'A default error occurred' }
+    };
+    const errorObj = Object.assign({}, defaultErr, err)
+    return res.status(errorObj.status).send(errorObj.message)
+});
+
+//server listen
 app.listen(port, () => {
     console.log(`Express is listening on port ${port}`)
 });
