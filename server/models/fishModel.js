edca773c6cf046496cfe0ea507f3851891f239d8
Eric Hagen <ericjameshagen@gmail.com>
2023-08-15 13:53:49 -0600

    remove duplicate identifier

--- server/models/fishModel.js
+++ server/models/fishModel.js
@@ -1,11 +1,7 @@
 require("dotenv").config();
 
-const { Pool } = require("pg");
-const PG_URL = process.env.DB_URL;
-
-
 const PG_URL = process.env.DBURL;
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: PG_URL,
 });
